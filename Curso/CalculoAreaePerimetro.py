0c5bbb31b805d99a398cb3a753230a196342f94f
Áurea Simões <aurea.lazsn@gmail.com>
2022-03-25 10:28:12 -0300

    Atualização exercicio 1.1 - Introdução a Python Coursera

---
+++ Curso/CalculoAreaePerimetro.py
@@ -0,0 +1,5 @@
+LadoString = input("Digite o valor correspondente ao lado de um quadrado")
+LadoInt = int(LadoString)
+Perimetro = LadoInt * 4
+Area = LadoInt ** 2
+print("perímetro: ", Perimetro, "-", "area: ", Area)
